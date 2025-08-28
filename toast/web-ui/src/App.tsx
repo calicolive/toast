f547a8a380a1bd09f5d3fc79fd455a36af025adb
calicolive <calicodeep@gmail.com>
2025-08-28 12:20:25 -0500

    created temp layout for parameters and top menu

--- toast/web-ui/src/App.tsx
+++ toast/web-ui/src/App.tsx
@@ -3,7 +3,18 @@ import type { Component } from "solid-js";
 const App: Component = () => {
   return (
     <>
-      <main class="min-h-screen w-screen bg-emerald-300"></main>
+      <main class="min-h-screen w-screen bg-emerald-300">
+        {/* this is where the top menu will be. this will be where presets are stored. */}
+        <header class="container mx-auto max-w-2xl">
+          <nav class="sticky top-0 flex justify-between p-6 text-center ">
+            <div class="text-xl font-bold">Toast</div>
+            <div>Preset menu</div>
+            <div></div>
+          </nav>
+        </header>
+        {/* this is where the parameters will go */}
+        <div></div>
+      </main>
     </>
   );
 };
